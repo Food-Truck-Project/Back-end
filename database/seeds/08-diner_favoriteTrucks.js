5bc019d0a2091123fc756aac4cba0e10829cda11
bhawnishk1 <mrbhawnish@gmail.com>
2020-11-17 16:31:44 -0800

    something changed

--- database/seeds/08-diner_favoriteTrucks.js
+++ database/seeds/08-diner_favoriteTrucks.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
     return knex('diner_favoriteTrucks').del()
       .then(function () {
         // Inserts seed entries
-        return knex('diner_FavoriteTrucks').insert([
+        return knex('diner_favoriteTrucks').insert([
           {user_id: 1, truck_id: 1},
           {user_id: 1, truck_id: 2},
           {user_id: 2, truck_id: 1},
