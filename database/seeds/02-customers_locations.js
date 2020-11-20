5197b3a15ec5b070258866b8b8e8c5fe625073a6
bhawnishk1 <mrbhawnish@gmail.com>
2020-11-19 16:16:23 -0800

    some changes

--- database/seeds/02-customers_locations.js
+++ database/seeds/02-customers_locations.js
@@ -6,10 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('customers_locations').insert([
         {id: 1, latitude: 41.385063, longitude: 2.173404, user_id: 1},
-        {id: 2, latitude: 56.263920, longitude: 9.501785, user_id: 2},
-        {id: 3, latitude: 56.5351, longitude: -0.02127758, user_id: 3},
-        {id: 4, latitude: 59.5351, longitude: -0.0127758, user_id: 5},
-        {id: 5, latitude: 51.507351, longitude: -0.17758, user_id: 4}
+        {id: 2, latitude: 56.263920, longitude: 9.501785, user_id: 2}
       ]);
     });
 };
