89cee6e1776e829555e6b0883344b71f0c2abc2e
bhawnishk1 <mrbhawnish@gmail.com>
2020-11-19 16:23:09 -0800

    some changes

--- database/migrations/20201118111652_trucks.js
+++ database/migrations/20201118111652_trucks.js
@@ -28,7 +28,7 @@ exports.up = async function(knex) {
     tbl.float('latitude').defaultTo(0.0)
     tbl.float('longitude').defaultTo(0.0)
     tbl.string('physical_address').defaultTo("Not specified")
-    tbl.integer("user_id").unique().unsigned()
+    tbl.integer("user_id").unsigned()
     .references("id")
     .inTable("users")
     .onDelete("RESTRICT")
